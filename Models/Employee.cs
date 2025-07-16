c613f6c970f1b5c393cc5a8ab0bc6a7a24ff54e6
tgbclskn <potassium.11k@gmail.com>
2025-07-16 01:50:31 -0700

    update

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -3,11 +3,11 @@
     public class Employee
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public int CompanyId { get; set; }
-        public string PasswordHash { get; set; }
-        public string Email { get; set; }
+        public string? PasswordHash { get; set; }
+        public string? Email { get; set; }
         public bool IsActive { get; set; }       
         public DateTime CreatedDate { get; set; } = DateTime.Now;
 
