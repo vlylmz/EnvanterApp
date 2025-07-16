874d2796d58e8516825a7ed4303b2045f4ff8c9a
Veli Yılmaz <yilmazv2001@gmail.com>
2025-07-16 14:56:42 +0300

    firma çalışan bilgisayar yazılım login ekranını değişti.

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}")
+    pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
