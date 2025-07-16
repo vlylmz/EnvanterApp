7ede69ddecae2af2d6e6cfa9f7471adc16cf25bb
Veli Yılmaz <yilmazv2001@gmail.com>
2025-07-16 11:24:16 +0300

    Çalışanlar sekmesi tamamlandı.

---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CompanyId { get; set; }
+        public string PasswordHash { get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }       
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+    }
+}
